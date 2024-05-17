fc073edcce76532bdddb08b5452b205700362ffd
dushan6693 <dushanmadushankabeligala9@gmail.com>
2024-05-17 15:47:13 +0530

    final updates

--- sign up.php
+++ sign up.php
@@ -1,18 +1,19 @@
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/sign up.css">
+    <link rel="stylesheet" href="sign up.css">
 </head>
 <body>
     <section class="container">
         <header>Sign Up</header>
         <p class="title">Already have an account? 
-            <a href="sign in.html">Log In</a>
+            <a rel="noopener noreferrer" href="#" class="">Log In</a>
         </p>
-        <form class="form" action="#">
+        <form class="form" action="insert.php" method="post">
             <div class="row g-3">
                 <div class="input-container col-lg-6">
                     <label for="first-name" class="form-label">First Name</label>
@@ -21,7 +22,7 @@
                 </div>
                 <div class="input-container col-lg-6">
                     <label for="last-name" class="form-label">Last Name</label>
-                    <input type="text" id="last name" class="form-control">
+                    <input type="text" id="last-name"  class="form-control">
                 </div>
             </div>
             <div class="row g-3">
@@ -32,7 +33,7 @@
                 </div>
                 <div class="input-container col-lg-6">
                     <label for="mobile number" class="form-label">Mobile Number</label>
-                    <input type="text" id="mobile number" class="form-control">
+                    <input type="text" id="mobile" class="form-control">
                 </div>
             </div>
             <div class="input-container col-lg-12">
@@ -47,7 +48,7 @@
                 </div>
                 <div class="input-container col-lg-6">
                     <label for="Confirm Your Password" class="form-label">Confirm Your Password</label>
-                    <input type="text" id="confirmpassword" class="form-control">
+                    <input type="password" id="re-password"  class="form-control">
                 </div>
             </div>
             <br>
